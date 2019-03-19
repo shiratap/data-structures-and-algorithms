63ea57d164305d2652e9383762c7c0a3feaae381
shiratap <spencerhirata@gmail.com>
2019-03-19 16:46:44 -0700

    arrayShift done

---
+++ code-challenges/401/arrayShift/README.md
@@ -0,0 +1,11 @@
+# Insert and shift middle index of array
+Inserts a value into the middle of an array
+
+## Challenge
+Add a value into the middle of an array
+
+## Approach & Efficiency
+We took the approach where we take the array and create a copy + 1 to it, and copied for each element in the array into the new array except the middle index, where the new value is assigned at that index.
+
+## Solution
+![Screenshot](../assets/array_shift.jpg)
---
+++ code-challenges/401/arrayShift/array-shift.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const insertShiftArray = function(array, value){
+
+  if(checkIfInvalidInput(array) || array.length === 0){return null};
+
+  const newArray = new Array(array.length + 1);
+  const midpoint = findMidpoint(array.length);
+  for(let i = 0; i < newArray.length; i++){
+    if(i < midpoint){
+      newArray[i] = array[i];
+    }
+    else if(i === midpoint){
+      newArray[i] = value;
+    }
+    else{
+      newArray[i] = array[i - 1];
+    }
+  }
+  return newArray;
+};
+
+let findMidpoint = function(arrayLength){
+  if(arrayLength % 2 === 1){
+    return (arrayLength/2) + .5;
+  }
+  else{
+    return (arrayLength/2);
+  }
+};
+
+let checkIfInvalidInput = function(input){
+  return (typeof input !== typeof []);
+};
+
+module.exports = insertShiftArray;
