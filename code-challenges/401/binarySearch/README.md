0332a09128fc09f5b0c18e88e4522054bff4f3b0
shiratap <spencerhirata@gmail.com>
2019-03-20 22:12:59 -0700

    Challenge done

---
+++ code-challenges/401/binarySearch/README.md
@@ -0,0 +1,11 @@
+# Binary Search
+Finds index of any input value, if not returns -1
+
+## Challenge
+Binary search, figure out if and where the value inputted into the second paramater is in the first parameter array. The return value is the index of that value in the array
+
+## Approach & Efficiency
+The effeciency of the code is based proportional to the array list, but like O(arraylist/n);
+
+## Solution
+![Screenshot](../assets/array-binary-search.jpg)
---
+++ code-challenges/401/binarySearch/__tests__/array-binary-search.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const binarySearch = require('../array-binary-search.js');
+
+describe('When looking for a value and the value is in the array', () => {
+  it('Should return the index found for that value within the array', () => {
+    let array = [1,2,3,4,5,6,7,8,9,10];
+    let value = 5;
+    expect(binarySearch(array,value)).toEqual(4);
+  });
+});
+
+describe('If the array is not sorted and the end values do not contain the value' , () => {
+  it('Should return -1', () => {
+    let array = [3,4,5];
+    let value = 10;
+    expect(binarySearch(array,value)).toBe(-1);
+  });
+});
+
+describe('If the first parameter is not an array', () => {
+  it('Should return some sort of error or null', () => {
+    let array = 'This is not an array';
+    let value = 4;
+    expect(binarySearch(array,value)).toBeNull();
+  });
+});
+
+describe('If the second parameter is not an array', () => {
+  it('Should return -1', () => {
+    let array = [3,4,5];
+    let value = 'haHAA';
+    expect(binarySearch(array,value)).toBe(-1);
+  });
+});
