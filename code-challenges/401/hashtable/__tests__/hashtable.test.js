4de98751f354e85baf091f6ed5f5d31643eefdef
shiratap <spencerhirata@gmail.com>
2019-04-28 20:19:44 -0700

    no tests

---
+++ code-challenges/401/hashtable/__tests__/hashtable.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const HT = require('../hashtable.js');
+
+
+
+// #### Adding a key/value to your hashtable results in the value being in the data structure
+// #### Retrieving based on a key returns the value stored
+// #### Successfully returns null for a key that does not exist in the hashtable
+// #### Successfully handle a collision within the hashtable
+// #### Successfully retrieve a value from a bucket within the hashtable that has a collision
+// #### Successfully hash a key to an in-range value
+
+
+
+describe('HT tests', () => {
+  it('should be alive', () => {
+    expect(true).toBeTruthy;
+  });
+
+  xit('Adding a key/value to your hashtable results in the value being in the data structure', () => {
+
+  });
+  it('Retrieving based on a key returns the value stored', () => {
+
+  });
+  xit('Successfully returns null for a key that does not exist in the hashtable', () => {
+
+  });
+  xit('Successfully handle a collision within the hashtable', () => {
+
+  });
+  xit('Successfully retrieve a value from a bucket within the hashtable that has a collision', () => {
+
+  });
+  xit('Successfully hash a key to an in-range value', () => {
+
+  });
+
+});
